6f47e4a31d3ad1a194dbabd29a44abd85e541817
federicomaffei <federico.maffei@gmail.com>
2014-07-16 23:18:20 +0100

    temperature control, power saving mode and reset button implemented and tested

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -1,3 +1,76 @@
 describe("Thermostat", function() {
 
+  var thermostat;
+
+  beforeEach(function(){
+    thermostat = new Thermostat();
+  })
+
+  it("is initially at 20 degrees", function(){
+    expect(thermostat.temperature).toEqual(20);
+  })
+
+  it("has PSM in by default", function(){
+    expect(thermostat.powerSavingMode).toBe(true);
+  })
+
+  describe('Maximum temperature', function(){
+    it('is 25 degrees with PSM on', function(){
+      expect(thermostat.maximumTemperature()).toEqual(25);
+    })
+
+    it('is 25 degrees with PSM on', function(){
+      thermostat.powerSavingMode = false;
+      expect(thermostat.maximumTemperature()).toEqual(32);
+    })
+  })
+
+  describe('Minimum temperature', function(){
+    it('is 10 degrees', function(){
+      expect(thermostat.minimumTemperature()).toEqual(10);
+    })
+  })
+
+  describe('warmer', function (){
+
+    describe('PSM on', function(){
+      it('increases the temperature if < 25°', function(){
+        thermostat.warmer();
+        expect(thermostat.temperature).toEqual(21);
+      })
+      it('does not increase the temperature if >= 25°', function(){
+        thermostat.temperature = 25;
+        thermostat.warmer();
+        expect(thermostat.temperature).toEqual(25);
+      })
+    })
+
+    describe('PSM off', function(){
+      it('increases the temperature if < 32°', function(){
+        thermostat.powerSavingMode = false;
+        thermostat.warmer();
+        expect(thermostat.temperature).toEqual(21);
+      })
+      it('does not increase the temperature if >= 25°', function(){
+        thermostat.powerSavingMode = false;
+        thermostat.temperature = 25;
+        thermostat.warmer();
+        expect(thermostat.temperature).toEqual(26);
+      })
+    })
+  })
+
+  describe('cooler', function (){
+
+    it('decreases the temperature if > 10°', function(){
+      thermostat.cooler();
+      expect(thermostat.temperature).toEqual(19);
+    })
+    it('does not increase the temperature if <= 10°', function(){
+      thermostat.temperature = 10;
+      thermostat.cooler();
+      expect(thermostat.temperature).toEqual(10);
+    })
+  })
+
 });
